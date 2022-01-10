abfe88a5591088a91efa84fc2eea267c9ede71e9
mendespedro <windowsxpedro@gmail.com>
2022-01-10 19:15:17 -0300

    homepage dev done

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,16 @@ export default function Home() {
       </Head>
           <Header/>
           <main className={styles.main}>
-
+              <div className={styles.copyBanner}>
+                  <p className={styles.bannerText}>
+                      <em>
+                          Django My name is Pedro Mendes, and this website is currently under development.
+                      </em>
+                  </p>
+                  <p>
+                      <a className={styles.bannerButton}>Contact Me</a>
+                  </p>
+              </div>
           </main>
           <Footer/>
       </div>
