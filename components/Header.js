d90835a2454b10c11f3e9c97d720b6a138d55f19
mendespedro <windowsxpedro@gmail.com>
2022-01-13 17:21:04 -0300

    fixed mobile header hrefs

--- components/Header.js
+++ components/Header.js
@@ -57,17 +57,13 @@ const Header = ({workPageTargetShouldNotBeBlank}) => {
                 </div>
             </div>
             <div ref={wrapperRef} style={{display: isMobileMenuOpen ? 'initial' : 'none'}} className={styles.mobileMenu}>
-                <div onClick={() => router.push('/about')} className={styles.mobileMenuItem} style={{marginTop: '20px'}}>
-                    <a className={styles.mobileMenuText}>ABOUT</a>
-                </div>
-
-                <div onClick={() => router.push('/work')} className={styles.mobileMenuItem}>
+                <div onClick={() => router.push('/work')} className={styles.mobileMenuItem} style={{marginTop: '20px'}}>
                     <a className={styles.mobileMenuText}>WORK</a>
                 </div>
-                <div onClick={() => router.push('/projects')} className={styles.mobileMenuItem}>
-                    <a className={styles.mobileMenuText}>PROJECTS</a>
+                <div onClick={() => router.push('/#about')} className={styles.mobileMenuItem}>
+                    <a className={styles.mobileMenuText}>ABOUT</a>
                 </div>
-                <div onClick={() => router.push('/contact')}  className={styles.mobileMenuItem} style={{paddingBottom: 9}}>
+                <div onClick={() => router.push('/#contact')}  className={styles.mobileMenuItem} style={{paddingBottom: 9}}>
                     <a className={styles.mobileMenuText}>CONTACT</a>
                 </div>
             </div>
