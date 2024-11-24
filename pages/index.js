b1e638a1574be800f639a5afa4186f3f681f41b3
mendespedro <windowsxpedro@gmail.com>
2024-11-23 21:46:50 -0300

    commenting out irrelevant shit

--- pages/index.js
+++ pages/index.js
@@ -33,35 +33,35 @@ export default function Home() {
                       <p>
                           We specialize in turning ideas into Minimum Viable Products (MVPs) through expert software engineering. We pride ourselves on delivering rapid development without sacrificing quality, helping bring your vision to life efficiently and effectively.
                       </p>
-                      <div className={styles.projects}>
-                          <div onClick={() => router.push('/')}>
-                              <div className={styles.imageContainer}>
-                                  <img alt={'fetchly img'} src={'/fetchly.png'} />
-                              </div>
-                              <div className={styles.projectNameContainer}>
-                                  <span>Fetchly Labs</span>
-                                  <span style={{opacity: 0.7}}>Multiple projects</span>
-                              </div>
-                          </div>
-                          <div onClick={() => router.push('/')}>
-                              <div className={styles.imageContainer}>
-                                  <img alt={'travelstation img'} src={'/travelstation.png'} />
-                              </div>
-                              <div className={styles.projectNameContainer}>
-                                  <span>Travel Station</span>
-                                  <span style={{opacity: 0.7}}>Social Network for travelers</span>
-                              </div>
-                          </div>
-                          <div onClick={() => router.push('/')}>
-                              <div className={styles.imageContainer}>
-                                  <img alt={'eadguru img'} src={'/eadguru.png'} />
-                              </div>
-                              <div className={styles.projectNameContainer}>
-                                  <span>EadGuru</span>
-                                  <span style={{opacity: 0.7}}>Create your own learning platform</span>
-                              </div>
-                          </div>
-                      </div>
+                      {/*<div className={styles.projects}>*/}
+                      {/*    <div onClick={() => router.push('/')}>*/}
+                      {/*        <div className={styles.imageContainer}>*/}
+                      {/*            <img alt={'fetchly img'} src={'/fetchly.png'} />*/}
+                      {/*        </div>*/}
+                      {/*        <div className={styles.projectNameContainer}>*/}
+                      {/*            <span>Fetchly Labs</span>*/}
+                      {/*            <span style={{opacity: 0.7}}>Multiple projects</span>*/}
+                      {/*        </div>*/}
+                      {/*    </div>*/}
+                      {/*    <div onClick={() => router.push('/')}>*/}
+                      {/*        <div className={styles.imageContainer}>*/}
+                      {/*            <img alt={'travelstation img'} src={'/travelstation.png'} />*/}
+                      {/*        </div>*/}
+                      {/*        <div className={styles.projectNameContainer}>*/}
+                      {/*            <span>Travel Station</span>*/}
+                      {/*            <span style={{opacity: 0.7}}>Social Network for travelers</span>*/}
+                      {/*        </div>*/}
+                      {/*    </div>*/}
+                      {/*    <div onClick={() => router.push('/')}>*/}
+                      {/*        <div className={styles.imageContainer}>*/}
+                      {/*            <img alt={'eadguru img'} src={'/eadguru.png'} />*/}
+                      {/*        </div>*/}
+                      {/*        <div className={styles.projectNameContainer}>*/}
+                      {/*            <span>EadGuru</span>*/}
+                      {/*            <span style={{opacity: 0.7}}>Create your own learning platform</span>*/}
+                      {/*        </div>*/}
+                      {/*    </div>*/}
+                      {/*</div>*/}
                       <div className={styles.separator} id={'contact'}/>
                       <h1 className={styles.secondaryHeader} >Contact</h1>
                       <p><a className={'link'} href={emailUrl} target={'_blank'}>contact@mendespedro.dev</a></p>
@@ -105,21 +105,21 @@ export default function Home() {
                               CI/CD
                           </div>
                       </div>
-                      <div className={styles.rightMenuHeader} style={{marginTop: '40px'}}>
-                          <h2>Open Source Contributions</h2>
-                      </div>
-                      <div className={styles.rightMenuText}>
-                          <p><a href={'https://github.com/skryukov/turbo-mount'} target={'_blank'}>Turbo Mount</a></p>
-                          <p>2024</p>
-                      </div>
-                      <div className={styles.rightMenuText}>
-                          <p><a href={djangoRepoUrl} target={'_blank'}>Django</a></p>
-                          <p>2021</p>
-                      </div>
-                      <div className={styles.rightMenuText}>
-                          <p><a href={reactAutocomplete} target={'_blank'}>React Google Autocomplete</a></p>
-                          <p>2020</p>
-                      </div>
+                      {/*<div className={styles.rightMenuHeader} style={{marginTop: '40px'}}>*/}
+                      {/*    <h2>Open Source Contributions</h2>*/}
+                      {/*</div>*/}
+                      {/*<div className={styles.rightMenuText}>*/}
+                      {/*    <p><a href={'https://github.com/skryukov/turbo-mount'} target={'_blank'}>Turbo Mount</a></p>*/}
+                      {/*    <p>2024</p>*/}
+                      {/*</div>*/}
+                      {/*<div className={styles.rightMenuText}>*/}
+                      {/*    <p><a href={djangoRepoUrl} target={'_blank'}>Django</a></p>*/}
+                      {/*    <p>2021</p>*/}
+                      {/*</div>*/}
+                      {/*<div className={styles.rightMenuText}>*/}
+                      {/*    <p><a href={reactAutocomplete} target={'_blank'}>React Google Autocomplete</a></p>*/}
+                      {/*    <p>2020</p>*/}
+                      {/*</div>*/}
                   </div>
               </div>
           </main>
